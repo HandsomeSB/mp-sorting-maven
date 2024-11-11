40e7d845671f5e14577324661b04ec8ee182c322
HandsomeSB <zhuyiyun060209@gmail.com>
2024-11-11 16:22:58 -0600

    Finished quicksort

--- src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
@@ -1,6 +1,9 @@
 package edu.grinnell.csc207.sorting;
 
 import java.util.Comparator;
+import java.util.Random;
+
+import edu.grinnell.csc207.util.ArrayUtils;
 
 /**
  * Something that sorts using Quicksort.
@@ -55,6 +58,35 @@ public class Quicksorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    sort(values, 0, values.length);
   } // sort(T[])
+
+  private void sort(T[]values, int lowerBound, int higherBound) { 
+    if(lowerBound < higherBound - 1) { 
+      int pivotIndex = sortPartition(values, lowerBound, higherBound);
+
+      sort(values, lowerBound, pivotIndex);
+      sort(values, pivotIndex + 1, higherBound);
+    }
+  }
+
+  private int sortPartition(T[] values, int lowerBound, int higherBound) { 
+    Random rand = new Random();
+    int pivot = rand.nextInt(lowerBound, higherBound);
+
+    ArrayUtils.swap(values, pivot, higherBound - 1);
+
+    // values[lowerBound:leftPartIndex] < values[pivot]
+    int leftPartIndex = lowerBound;
+    for(int i = lowerBound; i < higherBound - 1; ++i) { 
+      if(order.compare(values[i], values[higherBound - 1]) < 0) { 
+        ArrayUtils.swap(values, leftPartIndex, i);
+        leftPartIndex++;
+      }
+    }
+
+    ArrayUtils.swap(values, higherBound - 1, leftPartIndex);
+
+    return leftPartIndex; 
+  }
 } // class Quicksorter
