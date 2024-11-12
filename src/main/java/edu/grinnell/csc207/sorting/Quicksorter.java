ddeba751c122ab826393a54111e5773b39a88770
HandsomeSB <zhuyiyun060209@gmail.com>
2024-11-11 23:40:41 -0600

    Added comments and renamed sortPartition to partition

--- src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/Quicksorter.java
@@ -24,6 +24,11 @@ public class Quicksorter<T> implements Sorter<T> {
    */
   Comparator<? super T> order;
 
+  /**
+   * Random number generator used to pick pivot.
+   */
+  Random rand = new Random();
+
   // +--------------+------------------------------------------------
   // | Constructors |
   // +--------------+
@@ -61,32 +66,49 @@ public class Quicksorter<T> implements Sorter<T> {
     sort(values, 0, values.length);
   } // sort(T[])
 
+  /**
+   * Sort an array in place using Quicksort.
+   * @param values Array of values
+   * @param lowerBound Start of sorting section
+   * @param higherBound End of sorting section
+   */
   private void sort(T[]values, int lowerBound, int higherBound) { 
     if(lowerBound < higherBound - 1) { 
-      int pivotIndex = sortPartition(values, lowerBound, higherBound);
+      int pivotIndex = partition(values, lowerBound, higherBound);
 
       sort(values, lowerBound, pivotIndex);
       sort(values, pivotIndex + 1, higherBound);
-    }
+    } // if at least 2 values between low and high
   }
 
-  private int sortPartition(T[] values, int lowerBound, int higherBound) { 
-    Random rand = new Random();
+  /**
+   * Partition the values between lower bound and higher bound.
+   * Chooses a pivot. Values less than the pivot will be moved 
+   * towards the left, values greater than the pivot will be 
+   * moved towards the right. 
+   * @param values The list of values
+   * @param lowerBound Lower bound
+   * @param higherBound Higher bound
+   * @return Index of pivot / partition
+   */
+  private int partition(T[] values, int lowerBound, int higherBound) { 
     int pivot = rand.nextInt(lowerBound, higherBound);
 
+    //Move pivot to the end
     ArrayUtils.swap(values, pivot, higherBound - 1);
 
     // values[lowerBound:leftPartIndex] < values[pivot]
-    int leftPartIndex = lowerBound;
+    int leftPartIndex = lowerBound; 
     for(int i = lowerBound; i < higherBound - 1; ++i) { 
       if(order.compare(values[i], values[higherBound - 1]) < 0) { 
         ArrayUtils.swap(values, leftPartIndex, i);
         leftPartIndex++;
-      }
-    }
+      } // if value less than pivot
+    } // for values between lower and higher
 
+    //Move pivot back
     ArrayUtils.swap(values, higherBound - 1, leftPartIndex);
 
     return leftPartIndex; 
-  }
+  } // partition(T[], int, int)
 } // class Quicksorter
